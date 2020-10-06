b1cc20262e611a12d92e81aea6e4333b32085422
Sorin Dan <dansorin2006@gmail.com>
2020-10-06 22:02:20 +0300

    fix(master): Set custom ip address for redis server

--- src/main/java/com/testing/services/DBService.java
+++ src/main/java/com/testing/services/DBService.java
@@ -3,6 +3,7 @@ package com.testing.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.testing.utils.DBObject;
 import com.testing.utils.RedisCache;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,10 @@ import java.io.IOException;
 @Service
 public class DBService {
 
-    private RedisCache redis = new RedisCache("localhost", 6379);
+    @Value("${redis.ip.address:localhost}")
+    private String redisIpAddress;
+
+    private RedisCache redis = new RedisCache(redisIpAddress, 6379);
 
     public ResponseEntity retrieveDataFromDb (String body) throws IOException {
         var jsonBody = new ObjectMapper();
