328ee1ecd9ced06222c684f81a41a52e3b37c6e7
sorin86-dan <dansorin2006@gmail.com>
2022-09-19 07:56:29 +0300

    feat(master): Added Spring Cloud Contract tests; reversed ip to redis server

--- src/main/java/com/testing/controllers/DBController.java
+++ src/main/java/com/testing/controllers/DBController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 
 @RestController
 public class DBController {
--- src/main/java/com/testing/services/DBService.java
+++ src/main/java/com/testing/services/DBService.java
@@ -13,8 +13,8 @@ import java.io.IOException;
 @Service
 public class DBService {
 
-    private RedisCache redis = new RedisCache("127.0.0.1", 6379);
-//    private RedisCache redis = new RedisCache("172.0.0.4", 6379);
+//    private RedisCache redis = new RedisCache("127.0.0.1", 6379);
+    private RedisCache redis = new RedisCache("172.0.0.4", 6379);
 
     public ResponseEntity retrieveDataFromDb (String body) throws IOException {
         var jsonBody = new ObjectMapper();
