16559b11018a997ac9375ec6de0d8f3e1ace41ff
sorin86-dan <dansorin2006@gmail.com>
2022-11-04 11:02:03 +0200

    fix(master): Merged mulitple contract yaml files in one, ;ix cedogroovy dependencyssue, ;dded commentsÈ; added options to run service locallyÈ

--- src/main/java/com/testing/services/DBService.java
+++ src/main/java/com/testing/services/DBService.java
@@ -13,8 +13,11 @@ import java.io.IOException;
 @Service
 public class DBService {
 
-//    private RedisCache redis = new RedisCache("127.0.0.1", 6379);
-    private RedisCache redis = new RedisCache("172.0.0.4", 6379);
+    // Using service with Docker containers
+//    private RedisCache redis = new RedisCache("172.0.0.4", 6379);
+
+    // Using service locally
+    private RedisCache redis = new RedisCache("127.0.0.1", 6379);
 
     public ResponseEntity retrieveDataFromDb (String body) throws IOException {
         var jsonBody = new ObjectMapper();
